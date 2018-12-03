30fce6c2858c38d8da663d3048642841aec484c7
twostarxx <xingxin.hsing@gmail.com>
2018-12-03 20:21:49 +0800

    + style_transfer includes model

---
+++ style_transfer.py
@@ -0,0 +1,20 @@
+
+class Style_transfer(object):
+
+    def __init__(self,
+                 content_img_path,
+                 style_img_path,
+                 res_img_path,
+                 ):
+
+        self.content_img_path = content_img_path;
+        self.style_img_path = style_img_path;
+        self.res_img_path = res_img_path;
+
+
+    def loss(self):
+
+
+
+
+
